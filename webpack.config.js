8549312ba43057a840db24d0b0aa94a8488a7732
WE5T3 <1024427426@qq.com>
2021-07-28 15:25:13 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-  },
-};
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.[name].[contenthash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Hi , CGC',
+        template: 'src/assets/test.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+}
+
