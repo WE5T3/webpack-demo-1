0017e76f039bc9d26e4cf2f7ae37a159a2837a13
WE5T3 <1024427426@qq.com>
2021-08-09 21:14:42 +0800

    init v1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require('path')
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.[name].[contenthash].js',
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 8090,
-    },
-    devtool: 'inline-source-map',
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Hi , CGC',
-        template: 'src/assets/test.html'
-    })],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-}
+const base = require('./webpack.config.base.js')
 
+module.exports = {
+  ...base,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    port: 8090,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const path = require('path')
+const base = require('./webpack.config.base.js')
+
+module.exports = {
+  mode: 'production',
+  ...base,
+  plugins: [
+    ...base.plugins,
+    new MiniCssExtractPlugin({
+      filename: 'index.[contenthash].css',
+      chunkFilename: 'index.[contenthash].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+}
