67ede8f067e0a1aa2d72f9151924fbb95fc24600
WE5T3 <1024427426@qq.com>
2021-08-12 17:54:31 +0800

    img/lazy

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,47 @@ module.exports = {
       title: 'Hi , CGC',
       template: 'src/assets/test.html'
     }),
-  ]
+  ],
+  
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "stylus-loader",
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require('dart-sass')
+            }
+          }
+        ],
+      },
+    ],
+  },
 }
